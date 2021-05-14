636e5d9c95d16aa6d7e8bedce6b9b1a2adc5fe2c
designatedcoder <niko@designatedcoder.com>
2021-05-14 18:59:02 -0400

    added adminlte css and js, jquery cdn to app.blade.php and set up dashboard

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,13 +12,17 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.1/dist/css/adminlte.min.css">
 
         <!-- Scripts -->
         @routes
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.1/dist/js/adminlte.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.26.0/moment.min.js"></script>
         <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
-    <body class="bg-light">
+    <body class="bg-light hold-transition layout-fixed layout-footer-fixed">
         @inertia
     </body>
 </html>
