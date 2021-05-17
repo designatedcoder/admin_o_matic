1f219b885d0a19bfc7b4d4f340b19cc2d0b770c6
designatedcoder <niko@designatedcoder.com>
2021-05-17 08:46:02 -0400

    set up chart js for dashboard.vue

--- resources/js/Pages/Admins/Dashboard.vue
+++ resources/js/Pages/Admins/Dashboard.vue
@@ -802,6 +802,83 @@
     export default {
         components: {
             AdminLayout,
+        },
+        mounted() {
+            this.salesChart()
+            this.pieChart()
+        },
+        methods: {
+            salesChart() {
+                let ctx = document.getElementById('salesChart').getContext('2d')
+                let salesChartData = {
+                    datasets: [{
+                        label               : 'Digital Goods',
+                        backgroundColor     : 'rgba(60,141,188,0.9)',
+                        borderColor         : 'rgba(60,141,188,0.8)',
+                        pointRadius          : false,
+                        pointColor          : '#3b8bba',
+                        pointStrokeColor    : 'rgba(60,141,188,1)',
+                        pointHighlightFill  : '#fff',
+                        pointHighlightStroke: 'rgba(60,141,188,1)',
+                        data                : [28, 48, 40, 19, 86, 27, 90]
+                    }, {
+                        label               : 'Electronics',
+                        backgroundColor     : 'rgba(210, 214, 222, 1)',
+                        borderColor         : 'rgba(210, 214, 222, 1)',
+                        pointRadius         : false,
+                        pointColor          : 'rgba(210, 214, 222, 1)',
+                        pointStrokeColor    : '#c1c7d1',
+                        pointHighlightFill  : '#fff',
+                        pointHighlightStroke: 'rgba(220,220,220,1)',
+                        data                : [65, 59, 80, 81, 56, 55, 40]
+                    }],
+                    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                }
+                let salesChartOptions = {
+                    maintainAspectRatio : false,
+                    responsive : true,
+                    legend: {
+                        display: false
+                    },
+                    scales: {
+                        xAxes: [{
+                        gridLines : {
+                            display : false,
+                        }
+                        }],
+                        yAxes: [{
+                        gridLines : {
+                            display : false,
+                        }
+                        }]
+                    }
+                }
+                new Chart(ctx, {
+                    type: 'line',
+                    data: salesChartData,
+                    options: salesChartOptions
+                })
+            },
+            pieChart() {
+                let ctx = document.getElementById('pieChart').getContext('2d')
+                let pieChartData = {
+                    datasets: [{
+                        data: [700, 500, 400, 600, 300, 100],
+                        backgroundColor: ['#f56954', '#00a65a', '#f39c12', '#00c0ef', '#3c8dbc', '#d2d6de']
+                    }],
+                    labels: [ 'Chrome', 'IE', 'FireFox', 'Safari', 'Opera', 'Navigator'],
+                }
+                let pieChartOptions = {
+                    legend: {
+                        display: false
+                    }
+                }
+                new Chart(ctx, {
+                    type: 'doughnut',
+                    data: pieChartData,
+                    options: pieChartOptions
+                })
+            }
         }
     }
 </script>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -17,6 +17,7 @@
 
         <!-- Scripts -->
         @routes
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.bundle.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.1/dist/js/adminlte.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.26.0/moment.min.js"></script>
