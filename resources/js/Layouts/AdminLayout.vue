d947cb50f0ca59b394c6ad3327a3c56f780257a8
designatedcoder <niko@designatedcoder.com>
2021-05-18 21:48:22 -0400

    added working pre-loader

--- resources/js/Layouts/AdminLayout.vue
+++ resources/js/Layouts/AdminLayout.vue
@@ -3,9 +3,9 @@
         <div class="wrapper">
 
         <!-- Preloader -->
-        <!-- <div class="preloader flex-column justify-content-center align-items-center">
-            <img class="animation__wobble" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
-        </div> -->
+        <div class="preloader flex-column justify-content-center align-items-center">
+            <img class="animation__wobble" :src="'/storage/images/AdminLTELogo.png'" alt="AdminLTELogo" height="60" width="60">
+        </div>
 
         <!-- Navbar -->
             <NavBar />
@@ -48,6 +48,23 @@
             Footer,
             NavBar,
             SideBar,
+        },
+        mounted() {
+            this.init()
+        },
+        methods: {
+            init() {
+                let SELECTOR_LOADER = '.loader'
+                setTimeout(() => {
+                    let $loader = $(SELECT_LOADER)
+                    if($loader) {
+                        $loader.css('height', 0)
+                        setTimeout(() => {
+                            $loader.children().hide()
+                        }, 2000)
+                    }
+                }, 2000)
+            }
         }
     }
 </script>
