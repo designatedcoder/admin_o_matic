fed4f2271a6c5859bc985a5da250f36680398f01
designatedcoder <niko@designatedcoder.com>
2021-06-02 00:31:42 -0400

    added roles.index link to sidebar

--- resources/js/Components/SideBar.vue
+++ resources/js/Components/SideBar.vue
@@ -57,7 +57,7 @@
                                 </inertia-link>
                             </li>
                             <li class="nav-item">
-                                <inertia-link href="#" class="nav-link">
+                                <inertia-link :href="route('admin.roles.index')" class="nav-link">
                                     <i class="far fa-circle nav-icon"></i>
                                     <p>
                                         Roles
